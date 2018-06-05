9eb8babecfaa6b09f1255425e301ebccfb98d88f
ontofractal <ontofractal@protonmail.com>
2018-06-05 17:20:13 +0100

    add get_histo_minute fn and doctests

--- lib/cryptocomparex.ex
+++ lib/cryptocomparex.ex
@@ -45,9 +45,11 @@ defmodule Cryptocomparex do
 
   """
   def get_histo_day(%{fsym: _fsym, tsym: _tsym} = params) do
-    query = params
+    query =
+      params
       |> KeyTools.camelize_keys(true)
-      |> Enum.into(Keyword.new)
+      |> Enum.into(Keyword.new())
+
     get("/data/histoday", query: query)
   end
 
@@ -64,12 +66,48 @@ defmodule Cryptocomparex do
   extra_params	The name of your application (we recommend you send it) [Max character length: 50]
   sign	If set to true, the server will sign the requests (be default we don't sign them), this is useful for usage in smart contracts
 
+    ## Examples
+
+    iex> {:ok, %{body: body = %{data: data}}} = Cryptocomparex.get_histo_hour(%{fsym: "BTC", tsym: "USD"})
+    iex> is_list(data) and is_float(hd(data).high)
+    true
+
   """
   def get_histo_hour(%{fsym: _fsym, tsym: _tsym} = params) do
-    query = params
+    query =
+      params
       |> KeyTools.camelize_keys(true)
-      |> Enum.into(Keyword.new)
+      |> Enum.into(Keyword.new())
+
     get("/data/histohour", query: query)
   end
 
+  @doc """
+  Get open, high, low, close, volumefrom and volumeto from the hourly historical data. It uses BTC conversion if data is not available because the coin is not trading in the specified currency.
+
+  try_conversion	If set to false, it will try to get only direct trading values
+  fsym	REQUIRED The cryptocurrency symbol of interest [Max character length: 10]
+  tsym	REQUIRED The currency symbol to convert into [Max character length: 10]
+  e	The exchange to obtain data from (our aggregated average - CCCAGG - by default) [Max character length: 30]
+  aggregate	Time period to aggregate the data over (for daily it's days, for hourly it's hours and for minute histo it's minutes)
+  limit	The number of data points to return
+  to_ts	Last unix timestamp to return data for
+  extra_params	The name of your application (we recommend you send it) [Max character length: 50]
+  sign	If set to true, the server will sign the requests (be default we don't sign them), this is useful for usage in smart contracts
+
+    ## Examples
+
+    iex> {:ok, %{body: body = %{data: data}}} = Cryptocomparex.get_histo_minute(%{fsym: "BTC", tsym: "USD"})
+    iex> is_list(data) and is_float(hd(data).high)
+    true
+
+  """
+  def get_histo_minute(%{fsym: _fsym, tsym: _tsym} = params) do
+    query =
+      params
+      |> KeyTools.camelize_keys(true)
+      |> Enum.into(Keyword.new())
+
+    get("/data/histominute", query: query)
+  end
 end
