56a3b747148e49ad6dbf1b3d0d78325a70c7722c
ontofractal <ontofractal@protonmail.com>
2018-06-26 13:47:20 +0100

    add get_daily_avg test

--- test/cryptocomparex_test.exs
+++ test/cryptocomparex_test.exs
@@ -2,4 +2,21 @@ defmodule CryptocomparexTest do
   use ExUnit.Case
   doctest Cryptocomparex
 
+  test "gets and parses get_histo_day response" do
+    {:ok, %{body: body = %{data: data}}} =
+      Cryptocomparex.get_histo_day(%{fsym: "BTC", tsym: "USD"})
+
+    assert is_list(data)
+    assert is_float(hd(data).high)
+  end
+
+  test "gets and parses get_daily_avg response" do
+    {:ok, ndt} = NaiveDateTime.new(2018, 1, 1, 0, 0, 0)
+    to_ts = ndt |> DateTime.from_naive!("Etc/UTC") |> DateTime.to_unix()
+
+    {:ok, %{body: body}} =
+      Cryptocomparex.get_daily_avg(%{fsym: "BTC", tsym: "USD", to_ts: to_ts})
+
+    assert body["USD"] == 13406.61
+  end
 end
