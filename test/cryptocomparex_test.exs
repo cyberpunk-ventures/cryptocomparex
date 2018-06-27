33c30f344953ee166b9c8b9afa54fe7c57d16b35
ontofractal <ontofractal@protonmail.com>
2018-06-27 19:25:06 +0100

    update test

--- test/cryptocomparex_test.exs
+++ test/cryptocomparex_test.exs
@@ -16,11 +16,11 @@ defmodule CryptocomparexTest do
     {:ok, ndt} = NaiveDateTime.new(2018, 1, 1, 0, 0, 0)
     to_ts = ndt |> DateTime.from_naive!("Etc/UTC") |> DateTime.to_unix()
 
-    opts = %HistoOhlcvOpts{fsym: "BTC", tsym: "USD", to_ts: to_ts}
+    opts = %{fsym: "BTC", tsym: "USD", to_ts: to_ts}
 
     {:ok, %{body: body}} =
       Cryptocomparex.get_histo_daily_avg(opts)
 
-    assert body["USD"] == 13406.61
+    assert body.usd == 13406.61
   end
 end
