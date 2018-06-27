44f05b9c95bb80ee6b7c8c4b8436bde4c5873e22
ontofractal <ontofractal@protonmail.com>
2018-06-27 19:17:51 +0100

    fix bugs related to switch from map args to struct args

--- lib/cryptocomparex.ex
+++ lib/cryptocomparex.ex
@@ -65,6 +65,8 @@ defmodule Cryptocomparex do
   def get_histo_daily_ohlcvs(%HistoOhlcvOpts{fsym: _fsym, tsym: _tsym} = params) do
     query =
       params
+      |> Map.from_struct()
+      |> remove_nil_fields()
       |> KeyTools.camelize_keys(true)
       |> Enum.into(Keyword.new())
 
@@ -96,6 +98,8 @@ defmodule Cryptocomparex do
   def get_histo_hourly_ohlcvs(%HistoOhlcvOpts{fsym: _fsym, tsym: _tsym} = params) do
     query =
       params
+      |> Map.from_struct()
+      |> remove_nil_fields()
       |> KeyTools.camelize_keys(true)
       |> Enum.into(Keyword.new())
 
@@ -127,12 +131,13 @@ defmodule Cryptocomparex do
   def get_histo_minute_ohlcvs(%HistoOhlcvOpts{fsym: _fsym, tsym: _tsym} = params) do
     query =
       params
+      |> Map.from_struct()
+      |> remove_nil_fields()
       |> KeyTools.camelize_keys(true)
       |> Enum.into(Keyword.new())
 
     get("/data/histominute", query: query)
   end
-
 
   @doc """
   Get day average price. The values are based on hourly vwap data and the average can be calculated in different ways. It uses BTC conversion if data is not available because the coin is not trading in the specified currency. If tryConversion is set to false it will give you the direct data. If no toTS is given it will automatically do the current day. Also for different timezones use the UTCHourDiff param
@@ -161,5 +166,11 @@ defmodule Cryptocomparex do
       |> Enum.into(Keyword.new())
 
     get("/data/dayAvg", query: query)
+  end
+
+  defp remove_nil_fields(map) do
+    for {k,v} <- map, !is_nil(v), into: %{} do
+      {k, v}
+    end
   end
 end
