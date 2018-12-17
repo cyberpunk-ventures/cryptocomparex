6cd3b37b0b4f7315aafe9ae5e32667ba80126af9
ontofractal <ontofractal@protonmail.com>
2018-12-17 17:52:08 +0200

    add testing for idiomatic keys of multi price full responses

--- test/cryptocomparex_test.exs
+++ test/cryptocomparex_test.exs
@@ -6,8 +6,7 @@ defmodule CryptocomparexTest do
 
   test "gets and parses get_histo_day response" do
     opts = %HistoOhlcvsOpts{fsym: "BTC", tsym: "USD"}
-    {:ok, %{body: _body = %{data: data}}} =
-      Cryptocomparex.get_histo_daily_ohlcvs(opts)
+    {:ok, %{body: _body = %{data: data}}} = Cryptocomparex.get_histo_daily_ohlcvs(opts)
 
     assert is_list(data)
     assert is_float(hd(data).high)
@@ -19,8 +18,7 @@ defmodule CryptocomparexTest do
 
     opts = %{fsym: "BTC", tsym: "USD", to_ts: to_ts}
 
-    {:ok, %{body: body}} =
-      Cryptocomparex.get_histo_daily_avg(opts)
+    {:ok, %{body: body}} = Cryptocomparex.get_histo_daily_avg(opts)
 
     assert body.usd == 13406.61
   end
@@ -32,6 +30,80 @@ defmodule CryptocomparexTest do
 
     assert is_map(body.display.btc)
     assert is_map(body.raw.btc)
+
+    %{
+      high_24h: _,
+      last_trade_id: _,
+      low_24h: _,
+      volume_24h: _,
+      high_day: _,
+      supply: _,
+      total_volume_24h_to: _,
+      change_pct_day: _,
+      last_volume: _,
+      last_volume_to: _,
+      marketcap: _,
+      change_day: _,
+      flags: _,
+      volume_day: _,
+      from_symbol: _,
+      open_day: _,
+      high_hour: _,
+      volume_day_to: _,
+      last_market: _,
+      volume_hour: _,
+      volume_24h_to: _,
+      market: _,
+      total_volume_24h: _,
+      change_pct_24h: _,
+      type: _,
+      volume_hour_to: _,
+      low_hour: _,
+      to_symbol: _,
+      low_day: _,
+      open_24h: _,
+      open_hour: _,
+      price: _,
+      last_update: _,
+      change_24h: _
+    } = body.raw.btc.usd
+
+    %{
+      high_24h: _,
+      last_trade_id: _,
+      low_24h: _,
+      volume_24h: _,
+      high_day: _,
+      supply: _,
+      total_volume_24h_to: _,
+      change_pct_day: _,
+      last_volume: _,
+      last_volume_to: _,
+      marketcap: _,
+      change_day: _,
+      flags: _,
+      volume_day: _,
+      from_symbol: _,
+      open_day: _,
+      high_hour: _,
+      volume_day_to: _,
+      last_market: _,
+      volume_hour: _,
+      volume_24h_to: _,
+      market: _,
+      total_volume_24h: _,
+      change_pct_24h: _,
+      type: _,
+      volume_hour_to: _,
+      low_hour: _,
+      to_symbol: _,
+      low_day: _,
+      open_24h: _,
+      open_hour: _,
+      price: _,
+      last_update: _,
+      change_24h: _
+    } = body.display.btc.usd
   end
 
   test "get ohlcv with multiple granularities" do
