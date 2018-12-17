a419ca897451a0cdfa7327c3cc407879189a25cb
ontofractal <ontofractal@protonmail.com>
2018-12-17 16:03:52 +0200

    add test for get ohlcvs with multiple granularity options

--- test/cryptocomparex_test.exs
+++ test/cryptocomparex_test.exs
@@ -2,6 +2,7 @@ defmodule CryptocomparexTest do
   use ExUnit.Case
   doctest Cryptocomparex
   alias Cryptocomparex.HistoOhlcvsOpts
+  alias Cryptocomparex.Opts
 
   test "gets and parses get_histo_day response" do
     opts = %HistoOhlcvsOpts{fsym: "BTC", tsym: "USD"}
@@ -31,5 +32,19 @@ defmodule CryptocomparexTest do
 
     assert is_map(body.display.btc)
     assert is_map(body.raw.btc)
+  end
+
+  test "get ohlcv with multiple granularities" do
+    opts_day = %Opts{fsym: "BTC", tsym: "USD", granularity: :day}
+    opts_hour = %Opts{fsym: "BTC", tsym: "USD", granularity: :hour}
+    opts_minute = %Opts{fsym: "BTC", tsym: "USD", granularity: :minute}
+
+    {:ok, %{body: body_day}} = Cryptocomparex.get_ohlcvs(opts_day)
+    {:ok, %{body: body_hour}} = Cryptocomparex.get_ohlcvs(opts_hour)
+    {:ok, %{body: body_minute}} = Cryptocomparex.get_ohlcvs(opts_minute)
+
+    assert %{high: _, low: _} = hd(body_day.data)
+    assert %{high: _, low: _} = hd(body_hour.data)
+    assert %{high: _, low: _} = hd(body_minute.data)
   end
 end
